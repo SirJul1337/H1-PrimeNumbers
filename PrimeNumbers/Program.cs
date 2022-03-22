b07a111dca9621ddffadb3a7c2c9698dbdc829ec
nj <niklas@julit.dk>
2022-03-22 10:34:54 +0100

    Add project files.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter an integer to check all prime numbers from 0-X: ");
+            int input = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= input; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if(i%j == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(i);
+                }
+            }
+
+                       
+        }
+    }
+}
